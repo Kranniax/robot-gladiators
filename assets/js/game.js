b106a885f8ca20c8ac7a5ef1391c9f2e1fdb0701
Shane Wade <shanebramble96@hotmail.com>
2022-08-11 18:28:50 -0500

    included console log testing for different scenarios

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-alert("This is an alert! JavaScript is running!");
+var playerName = prompt("What is your robot's name?");
+
+console.log(playerName);
+console.log(10+10);
+console.log("Our robots name is: " + playerName );
+
+function fight() {
+  alert("The fight has begun!");
+}
+
+// fight();
