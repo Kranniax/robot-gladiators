eb09f1546fd2c16497a001e3bbf6b01fd6df5ff5
Shane Wade <shanebramble96@hotmail.com>
2022-08-29 18:44:28 -0500

    changed switch cases to integer responses

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,7 +8,7 @@ var fightOrSkip = function () {
   var promptFight = prompt(
     "Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose."
   );
-  
+
   if (promptFight === "" || promptFight === null) {
     alert("You need to provide a valid answer! Please try again.");
     return fightOrSkip();
@@ -38,7 +38,7 @@ var fight = function (enemy) {
   // repeat and execute as long as the enemy-robot is alive.
   while (playerInfo.health > 0 && enemy.health > 0) {
     // ask player if they'd like to fight or skip using fightOrSkip Function.
-    if (fightOrSkip()){
+    if (fightOrSkip()) {
       break;
     }
 
@@ -164,19 +164,18 @@ var endGame = function () {
 var shop = function () {
   // ask player what they's like to do
   var shopOptionPrompt = prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
   );
+  shopOptionPrompt = parseInt(shopOptionPrompt);
+
   switch (shopOptionPrompt) {
-    case "REFILL":
-    case "refill":
+    case 1:
       playerInfo.refillHealth();
       break;
-    case "UPGRADE":
-    case "upgrade":
+    case 2:
       playerInfo.upgradeAttack();
       break;
-    case "LEAVE":
-    case "leave":
+    case 3:
       alert("Leaving the store.");
       // do nothing, so function will end.
       break;
